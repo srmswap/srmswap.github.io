f960dfd13c14596f53c6e8e5e197908a9158980e
fs <xinshuizhang@gmail.com>
2020-11-10 19:46:28 +0800

    Updates

---
+++ precache-manifest.762cef7695cb74820b147fb5d0a7dca6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adc98f2aaed494eaf56fe3a78263cd9",
+    "url": "/srmswap.github.io/index.html"
+  },
+  {
+    "revision": "7f839b7642458f0c1636",
+    "url": "/srmswap.github.io/static/css/2.b5a80ab8.chunk.css"
+  },
+  {
+    "revision": "ff184b8090fd3a9aea8d",
+    "url": "/srmswap.github.io/static/css/main.2a3a3d4e.chunk.css"
+  },
+  {
+    "revision": "7f839b7642458f0c1636",
+    "url": "/srmswap.github.io/static/js/2.97424c1b.chunk.js"
+  },
+  {
+    "revision": "6ff87620e4f720e979dad85ffac87bda",
+    "url": "/srmswap.github.io/static/js/2.97424c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff184b8090fd3a9aea8d",
+    "url": "/srmswap.github.io/static/js/main.4bfdd89b.chunk.js"
+  },
+  {
+    "revision": "6ffa97aa0a9fab9bac01",
+    "url": "/srmswap.github.io/static/js/runtime-main.8807816e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e12291b4d85f6130c209f019b24ad61e.js"
+  "/srmswap.github.io/precache-manifest.762cef7695cb74820b147fb5d0a7dca6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/srmswap.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
