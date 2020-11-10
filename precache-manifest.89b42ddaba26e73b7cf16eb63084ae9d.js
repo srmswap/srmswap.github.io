b3bff3aa764e27b16ffa1636b02b068878b8d59a
fs <xinshuizhang@gmail.com>
2020-11-10 11:14:35 +0800

    Updates

---
+++ precache-manifest.89b42ddaba26e73b7cf16eb63084ae9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21e5b12467ac0fc08177659bc224c7e",
+    "url": "/srmswap.github.io/index.html"
+  },
+  {
+    "revision": "7f839b7642458f0c1636",
+    "url": "/srmswap.github.io/static/css/2.b5a80ab8.chunk.css"
+  },
+  {
+    "revision": "4d8167d6815401889f6f",
+    "url": "/srmswap.github.io/static/css/main.2a3a3d4e.chunk.css"
+  },
+  {
+    "revision": "7f839b7642458f0c1636",
+    "url": "/srmswap.github.io/static/js/2.97424c1b.chunk.js"
+  },
+  {
+    "revision": "6ff87620e4f720e979dad85ffac87bda",
+    "url": "/srmswap.github.io/static/js/2.97424c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8167d6815401889f6f",
+    "url": "/srmswap.github.io/static/js/main.880fe6cf.chunk.js"
+  },
+  {
+    "revision": "6ffa97aa0a9fab9bac01",
+    "url": "/srmswap.github.io/static/js/runtime-main.8807816e.js"
+  }
+]);
