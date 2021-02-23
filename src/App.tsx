a9f656b11763f2af45643012244356b0140f0149
SensyCheung <xinshuizhang@gmail.com>
2021-02-23 15:43:08 +0800

    modify info

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className="Banner">
         <div className="Banner-description">
-          SRMSwap暂未完成代码审计，请自行承担风险(需翻墙使用)。
+          请自行承担风险(需翻墙使用)
         </div>
       </div>
       <Routes />
